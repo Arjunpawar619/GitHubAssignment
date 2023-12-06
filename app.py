11e796a22394656516a30ff63359bfa37d9f77d7
Arjun Pawar <arjunp6713@gmail.com>
2023-12-06 17:28:11 -0500

    Add Course

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ st.title("Campusx")
 c1 , c2 = st.columns(1)
 
 with c1:
-    st.write(" abcd efg")  
+    st.write(" abcd efg") 
+
+st.header("Courses")
+st.subheader("Data Analytics")
+st.subheader("DSA")
+st.subheader("OPP")
